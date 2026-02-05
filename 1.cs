fde4c8ca73d8998da4ca0d55c5b860b82d2f5c2a
USER <USER@USER>
2026-02-05 20:27:09 +0700

    Добавьте файлы проекта.

---
+++ 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program {
+  static int Main() {
+
+  int a = 0;
+  int n = 0;
+  int index = 0;
+  int result = 1;
+
+  Console.Write("a = ");
+  a = int.Parse(Console.ReadLine());
+
+  Console.Write("n = ");
+  n = int.Parse(Console.ReadLine());
+
+  for (index = 0; index < n; index++)
+  {
+    result = result * a;
+  }
+
+  Console.WriteLine(a + "^" + n + " = " + result);
+  return 0;
+  }
+}
